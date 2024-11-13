28dacfbf37a8cea63bd83c6dffb5206466dfbbc7
ravenn44 <169065512+ravenn44@users.noreply.github.com>
2024-11-13 08:01:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'aaiaiai',
+    'no pacaembu eu nunca vi',
+    'no morumbi nunca ta lá'
+)
+
+criaCartao(
+    'aiaaiiisiis',
+    'timinho',
+    'de fdp'
+)
+
+criaCartao(
+    'aiaaiaiai',
+    'não tem',
+    'torcida p apoiarrr'
+)
+
+criaCartao(
+    'aiiaaiia',
+    'EIIII',
+    'CADE VOCE?, CADE VOCEEEEEE?'
+)
